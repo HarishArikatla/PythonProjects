c1113aef048539eb2d2d23e535d5cf5e45e8f8c3
HarishArikatla <70283151+HarishArikatla@users.noreply.github.com>
2020-12-04 16:18:01 +0530

    take screenshot

---
+++ take scereenshot.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 26 07:27:13 2020
+
+@author: sis
+"""
+
+
+import pyfiglet
+result=pyfiglet.figlet_format("HARISH ARIKATLA",font="bubble")
+print(result)
+
+                              
