bbbb1aa3cd6006bbee4a700994f8cb8abeb71aa8
HarishArikatla <70283151+HarishArikatla@users.noreply.github.com>
2020-12-04 16:15:33 +0530

    whatsup delivery msgs

---
+++ whatsup msgs delivery.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  8 07:45:17 2020
+
+@author: sis
+"""
+
+
+import pywhatkit as purepython
+purepython.sendwhatmsg("+919666664609","sent with pyhton", 11, 9)
